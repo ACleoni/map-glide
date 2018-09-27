3b5866d0ca6f3210f0714f5ba225121ff897a98e
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-09-27 17:12:12 -0400

    Adds logo container in partials to handle rendering of the logo

---
+++ SmartMap/src/components/partials/BtnContoller.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    View,
+    TouchableOpacity,
+    Stylesheet
+} from 'react-native';
+
+
+
+
+class BtnController extends Component {
+    render() {
+        const style = {
+            ...styles.container,
+
+        }
+    }
+};
+
+export default BtnController
